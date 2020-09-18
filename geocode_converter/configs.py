04c3b7be4439cf969437b822b051edbdc76eb413
Luis Carlos <luiscarlosfirminopinheiro@gmail.com>
2020-09-18 11:28:56 -0300

    configs are inside a class now

--- geocode_converter/configs.py
+++ geocode_converter/configs.py
@@ -2,9 +2,9 @@ import os
 from typing import Optional
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DOTENV_PATH = os.path.join(BASE_DIR, ".env")
 
-load_dotenv(DOTENV_PATH)
-
-API_KEY: Optional[str] = os.environ.get("API_KEY", None)
+class Configs:
+    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    DOTENV_PATH = os.path.join(BASE_DIR, ".env")
+    load_dotenv(DOTENV_PATH)
+    API_KEY: Optional[str] = os.environ.get("API_KEY", None)
